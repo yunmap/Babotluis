8ccc0629a6e729de3177fbb8375b4eb35a319fc3
yunmap <kalmpink@korea.ac.kr>
2017-11-10 20:53:12 +0900

    20171110_LUIS연결

--- app.js
+++ app.js
@@ -32,3 +32,13 @@ server.post('/api/messages', connector.listen());
 var bot = new builder.UniversalBot(connector, function (session) {
     session.send("You said: %s", session.message.text);
 });
+
+var LUIS_MODEL_URL = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/95842f66-e786-4384-859b-a42ae3c10094?subscription-key=f6092ff10fcc4d079fda3d5983410ad1&verbose=true&timezoneOffset=0&q=";
+var recognizer = new builder.LuisRecognizer(LUIS_MODEL_URL);
+bot.recognizer(recognizer);
+
+bot.dialog('help', function (session) {
+    session.endDialog("피자 주문을 도와드리겠습니다! 어떤 피자를 원하세요? \"치즈 피자 주문하겠습니다\"라고 말씀해주세요.");
+}).triggerAction({
+    matches: 'help'
+});
